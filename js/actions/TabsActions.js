abfb5d1e75b7d343c248c6aaeb5152c917fa29e5
Lakhan Mandloi <lakhan_m@tekditechnologies.com>
2017-12-07 16:13:49 +0530

    Cleanup Repo

--- js/actions/TabsActions.js
+++
@@ -1,9 +0,0 @@
-var alt = require('../support/alt');
-
-var TabsActions = function() {
-	return {
-		update: function(i, options) { this.dispatch({ i: i, options: options }); }
-	};
-};
-
-module.exports = alt.createActions(TabsActions());
